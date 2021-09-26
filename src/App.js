582645157ad47548521c9e65affa2f7ceade5530
maxfer03 <sandsferdinand@gmail.com>
2021-09-26 16:15:46 -0300

    finished initial styling

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <div className="welcome">
         <div className = "title">👋 'sup</div>
         <div className = "subtitle">I'm Max, webdev interested in blockchain. Connect your Ethereum wallet and wave at me! 😎</div>
-        <button>Wave at Me</button>
+        <button className ="waveButton">👋👋👋</button>
       </div>
     </div>
   );
