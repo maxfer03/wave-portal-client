e14c90474b0d8fb198c6c9db5d7edf01f010a2fc
maxfer03 <sandsferdinand@gmail.com>
2021-09-26 15:28:55 -0300

    added welcome text and button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="welcome">
+        <div className = "title">👋 'sup</div>
+        <div className = "subtitle">I'm Max, webdev interested in blockchain. Connect your Ethereum wallet and wave at me! 😎</div>
+        <button>Wave at Me</button>
+      </div>
     </div>
   );
 }
